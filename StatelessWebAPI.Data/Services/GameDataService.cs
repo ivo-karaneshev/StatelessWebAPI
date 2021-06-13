639de991ae235b80ea0d08210e1c61b0074c9b74
ivo-karaneshev <ikaraneshev95@gmail.com>
2021-06-14 02:41:04 +0300

    Add game data service.

---
+++ StatelessWebAPI.Data/Services/GameDataService.cs
@@ -0,0 +1,58 @@
+﻿using StatelessWebAPI.Data.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatelessWebAPI.Data.Services
+{
+    public class GameDataService : IGameDataService
+    {
+        private readonly GameDbContext _dbContext;
+
+        internal GameDataService(GameDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Game> BuyGame(int userId)
+        {
+            var game = new Game
+            {
+                State = State.Bought,
+                UserId = userId
+            };
+
+            await _dbContext.AddAsync(game);
+            await _dbContext.SaveChangesAsync();
+
+            return game;
+        }
+
+        public async Task<Game> ChangeGameState(int gameId, State state)
+        {
+            var game = await _dbContext.FindAsync<Game>(gameId);
+            if (game != null)
+            {
+                game.State = state;
+                await _dbContext.SaveChangesAsync();
+            }
+
+            return game;
+        }
+
+        public async Task<State?> GetGameState(int gameId)
+        {
+            var game = await _dbContext.FindAsync<Game>(gameId);
+            if (game == null)
+            {
+                return null;
+            }
+
+            return game.State;
+        }
+
+        public IQueryable<Game> GetUserGames(int userId)
+        {
+            return _dbContext.GameItems.Where(x => x.UserId == userId);
+        }
+    }
+}
---
+++ StatelessWebAPI.Data/Services/IGameDataService.cs
@@ -0,0 +1,38 @@
+﻿using StatelessWebAPI.Data.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatelessWebAPI.Data.Services
+{
+    public interface IGameDataService
+    {
+        /// <summary>
+        /// Creates a new game and sets its state to <see cref="State.Bought"/>.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>The newly created game.</returns>
+        Task<Game> BuyGame(int userId);
+
+        /// <summary>
+        /// Changes game state.
+        /// </summary>
+        /// <param name="gameId"></param>
+        /// <param name="state"></param>
+        /// <returns>The updated game or null if not found.</returns>
+        Task<Game> ChangeGameState(int gameId, State state);
+
+        /// <summary>
+        /// Gets game state.
+        /// </summary>
+        /// <param name="gameId"></param>
+        /// <returns>The state of the game or null if not found.</returns>
+        Task<State?> GetGameState(int gameId);
+
+        /// <summary>
+        /// Gets all games for a specific user.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>All games for the specified user.</returns>
+        IQueryable<Game> GetUserGames(int userId);
+    }
+}
